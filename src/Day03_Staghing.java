24a957d99ffb1cdbced4dc56319e8fe86c8af2d9
01Furkan <110833045+A-1407@users.noreply.github.com>
2023-11-22 14:20:21 +0300

    Add files via upload

---
+++ src/Day03_Staghing.java
@@ -0,0 +1,18 @@
+public class Day03_Staghing {
+    public static void main(String[] args) {
+
+        System.out.println("3. Gun 1. Deneme");
+        System.out.println("3. Gun 2. Deneme");
+        System.out.println("3. Gun 3. Branch Deneme");
+        System.out.println("3. Gun 4. Branch Deneme");
+        System.out.println("3. Gun 5. Branch'den master'a gecis Denemesi");
+        System.out.println("3. Gun 6. Master'dan branch'e gecis denemesi Deneme");
+        System.out.println("3. Gun 7. stash Deneme");
+        System.out.println("3. Gun 8. Stash Deneme");
+        System.out.println("3. Gun 9. Stash Deneme");
+        System.out.println("3. Gun 10. Stash Deneme");
+        System.out.println("3. Gun 11. Stash Deneme");
+
+
+    }
+}
